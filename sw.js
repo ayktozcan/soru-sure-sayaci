d4110ab14fc2b6d0241c7dd79baeda15f3e9153d
ayktozcan <ayktozcan@hotmail.com>
2026-04-17 09:45:19 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+});
